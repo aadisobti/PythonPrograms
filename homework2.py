b0111630fcd2aa2b56415a8e8bda118fdc540150
aadisobti <68819941+aadisobti@users.noreply.github.com>
2020-07-31 18:16:52 -0600

    Create homework2.py

---
+++ homework2.py
@@ -0,0 +1,8 @@
+import turtle
+my_screen = turtle.getscreen()
+my_turtle=turtle.Turtle()
+amount = int(input ('How many squares do you want?'))
+my_turtle.speed(0)
+for x in range(amount):
+  my_turtle.right(91)
+  my_turtle.forward(x*5)
