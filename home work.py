c35155616f989ba07afbd0d3702687f0f60ab085
aadisobti <68819941+aadisobti@users.noreply.github.com>
2020-07-31 18:15:47 -0600

    Create home work.py

---
+++ home work.py
@@ -0,0 +1,15 @@
+a=input("Please enter a adjective: ")
+b=input("Please enter a noun: ")
+c=input("Please enter a verb that ends in a -ed: ")
+d=input("Please enter a noun: ")
+e=input("Please enter a a verb that ends in a -ed: ")
+f=input("Please enter a noun that is not proper: ")
+g=input("Please enter a verb that ends in a -ed: ")
+h=input("Please enter a adjective: ")
+i=input("Please enter a animal: ")
+k=input("Please enter a funny verb like farting that ends in a -ing: ")
+
+
+
+
+print(f"The {a} {b} {c} over the {d} and then {e} around the neighborhood and met a {f} and then {g} into it but soon a {h} {i} put a stop to {b} by {k}") 
