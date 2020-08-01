fb9db8bfd4fd5d7a6e3f52c91d9412ed2c1e8fda
aadisobti <68819941+aadisobti@users.noreply.github.com>
2020-07-31 18:13:58 -0600

    Create background.py

---
+++ background.py
@@ -0,0 +1,27 @@
+import turtle
+my_screen = turtle.getscreen()
+t=turtle.Turtle()
+colors=['red', 'purple', 'blue',
+        'green', 'yellow', 'orange']
+for x in range(360):
+             t.pencolor=(f"{colors[0]}")
+             t.forward(100)
+             t.right(61)
+             t.pencolor=(f"{colors[1]}")
+             t.forward(100)
+             t.right(61)
+             t.pencolor=(f"{colors[2]}")
+             t.forward(100)
+             t.right(61)
+             t.pencolor=(f"{colors[3]}")
+             t.forward(100)
+             t.right(61)
+             t.pencolor=(f"{colors[4]}")
+             t.forward(100)
+             t.right(61)
+             t.pencolor=(f"{colors[5]}")
+             t.forward(100)
+             t.right(61)
+             
+
+
